6eea5aa69b97137ab6754b38eabc29b78baab6b3
Robert Rostohar <robert.rostohar@arm.com>
2022-08-30 14:54:30 +0200

    Update to use mbedTLS 3.1.0

--- source/google_iot.c
+++ source/google_iot.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Arm Limited. All rights reserved.
+ * Copyright (c) 2018-2022 Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -16,6 +16,7 @@
  * limitations under the License.
  */
 
+#define MBEDTLS_ALLOW_PRIVATE_ACCESS
 #include "google_iot.h"
 
 #define JWT_HEAD_RS256  "{\"alg\":\"RS256\",\"typ\":\"JWT\"}"
@@ -43,11 +44,12 @@ static void base64url (char *str, size_t ilen, size_t *olen) {
   *olen = j;
 }
 
-// Compute signature using ECSDA and SHA-256
+// Compute signature using ECDSA and SHA-256
 #if (GOOGLE_IOT_JWT_ES256 != 0)
 static int es256_sign (mbedtls_pk_context *pk,
                        const unsigned char *hash,
-                       unsigned char *sig) {
+                       unsigned char *sig,
+                       mbedtls_ctr_drbg_context *p_rng) {
   mbedtls_ecdsa_context ecdsa;
   mbedtls_mpi r, s;
   int rc;
@@ -63,8 +65,9 @@ static int es256_sign (mbedtls_pk_context *pk,
   }
 
   // Compute signature
-  rc = mbedtls_ecdsa_sign_det(&ecdsa.grp, &r, &s, &ecdsa.d,
-                              hash, 32, MBEDTLS_MD_SHA256);
+  rc = mbedtls_ecdsa_sign_det_ext(&ecdsa.grp, &r, &s, &ecdsa.d,
+                                  hash, 32, MBEDTLS_MD_SHA256,
+                                  mbedtls_ctr_drbg_random, p_rng);
   if (rc != 0) {
     goto exit;
   }
@@ -89,41 +92,19 @@ static int es256_sign (mbedtls_pk_context *pk,
 }
 #endif
 
-// Compute signature using RSASSA-PKCS1 and SHA-256
+// Compute signature using RSA-PKCS1 and SHA-256
 #if (GOOGLE_IOT_JWT_RS256 != 0)
 static int rs256_sign (mbedtls_pk_context *pk,
                        const unsigned char *hash,
-                       unsigned char *sig) {
-  mbedtls_entropy_context entropy;
-  mbedtls_ctr_drbg_context ctr_drbg;
-  const char *pers = "google_iot_jwt_sign";
+                       unsigned char *sig,
+                       mbedtls_ctr_drbg_context *p_rng) {
   int rc;
-
-  // Initialize objects
-  mbedtls_entropy_init(&entropy);
-  mbedtls_ctr_drbg_init(&ctr_drbg);
-
-  // Seeding random number generator
-  rc = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-                             (const unsigned char *)pers, strlen(pers));
-  if (rc != 0) {
-    goto exit;
-  }
 
   // Compute signature
   rc = mbedtls_rsa_pkcs1_sign(mbedtls_pk_rsa(*pk),
-                              mbedtls_ctr_drbg_random, &ctr_drbg,
-                              MBEDTLS_RSA_PRIVATE, MBEDTLS_MD_SHA256,
+                              mbedtls_ctr_drbg_random, p_rng,
+                              MBEDTLS_MD_SHA256,
                               32, hash, sig);
-  if (rc != 0) {
-    goto exit;
-  }
-
- exit:
-  // Clean-up
-  mbedtls_ctr_drbg_free(&ctr_drbg);
-  mbedtls_entropy_free(&entropy);
-
   return rc;
 }
 #endif
@@ -133,6 +114,9 @@ char *google_iot_jwt (const char *private_key,
                       const char *project_id,
                       unsigned int iat,
                       unsigned int exp) {
+  mbedtls_entropy_context entropy;
+  mbedtls_ctr_drbg_context ctr_drbg;
+  const char *pers = "google_iot_jwt_sign";
   mbedtls_pk_context pk;
   mbedtls_pk_type_t pk_type;
   unsigned char hash[32];
@@ -148,16 +132,27 @@ char *google_iot_jwt (const char *private_key,
   int rc;
 
   // Initialize objects
+  mbedtls_entropy_init(&entropy);
+  mbedtls_ctr_drbg_init(&ctr_drbg);
   mbedtls_pk_init(&pk);
+
+  // Seeding random number generator
+  rc = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+                             (const unsigned char *)pers, strlen(pers));
+  if (rc != 0) {
+    goto exit;
+  }
 
   // Load private key
   rc = mbedtls_pk_parse_key(&pk,
                             (const unsigned char *)private_key,
                             strlen(private_key) + 1,
-                            NULL, 0);
+                            NULL, 0,
+                            mbedtls_ctr_drbg_random, &ctr_drbg);
   if (rc != 0) {
     goto exit;
   }
+
   pk_type = mbedtls_pk_get_type(&pk);
 
   // Setup based on private key type
@@ -241,7 +236,7 @@ char *google_iot_jwt (const char *private_key,
     case MBEDTLS_PK_ECKEY:
       sig = malloc(sig_len);
       if (sig != NULL) {
-        rc = es256_sign(&pk, hash, sig);
+        rc = es256_sign(&pk, hash, sig, &ctr_drbg);
       } else {
         rc = -1;
       }
@@ -251,7 +246,7 @@ char *google_iot_jwt (const char *private_key,
     case MBEDTLS_PK_RSA:
       sig = malloc(sig_len);
       if (sig != NULL) {
-        rc = rs256_sign(&pk, hash, sig);
+        rc = rs256_sign(&pk, hash, sig, &ctr_drbg);
       } else {
         rc = -1;
       }
@@ -284,6 +279,8 @@ char *google_iot_jwt (const char *private_key,
   exit:
   // Clean-up
   mbedtls_pk_free(&pk);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_entropy_free(&entropy);
   free(sig);
   free(str);
   if (rc != 0) {
